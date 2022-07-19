934eabcea143a797c7da416a9ac93aecea735621
Timur Rakhmatullin <rax_5@mail.ru>
2022-07-20 00:03:49 +0500

    first commit

---
+++ src/main/java/rax/springpassaccess/SpringPassAccessApplication.java
@@ -0,0 +1,13 @@
+package rax.springpassaccess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPassAccessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPassAccessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rax/springpassaccess/SpringPassAccessApplicationTests.java
@@ -0,0 +1,13 @@
+package rax.springpassaccess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPassAccessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
